04c27f695c1838670061542b441481fc2d7354d6
constantinandrei <andrei.todirascu@gmail.com>
2022-06-21 14:57:57 +0200

    some style added to navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-        ma funziona sto coso?
-    </div>
+    <nav>
+        <div class="py-3 bg-black"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col flex-grow-0 flex-shrink-1 d-flex align-items-center justify-content-between">
+                    <div class="p-3">
+                        <img src="/img/dc-logo.png" alt=""/>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-center justify-content-between">
+                    <!-- <ul>
+                        <li v-for=""></li>
+                    </ul> -->
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -10,5 +24,7 @@
     }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+
 </style>
