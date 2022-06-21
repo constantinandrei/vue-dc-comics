4e6ac4be16e9525da3c76352f3c52d99d45c399a
constantinandrei <andrei.todirascu@gmail.com>
2022-06-21 15:22:22 +0200

    The NavBAr complete

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -8,10 +8,12 @@
                         <img src="/img/dc-logo.png" alt=""/>
                     </div>
                 </div>
-                <div class="col d-flex align-items-center justify-content-between">
-                    <!-- <ul>
-                        <li v-for=""></li>
-                    </ul> -->
+                <div class="col d-flex align-items-center justify-content-end">
+                    <ul class="nav">
+                        <li class="nav-item" v-for="(text, i) in navLinks" :key="i">
+                            <a class="nav-link" href="#">{{ text }}</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -20,11 +22,19 @@
 
 <script>
     export default {
-
+        name: 'TheNavbar',
+        data(){
+            return {
+                navLinks: ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP']
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
-
+a {
+    font-size: .9rem;
+    color: black;
+    font-weight: bold;
+}
 </style>
