4cd846cf980a0e2d8e17d83b7b9b3ac05f896a2f
constantinandrei <andrei.todirascu@gmail.com>
2022-06-22 15:39:51 +0200

    cards created

---
+++ src/components/MyCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="my-card">
+        <div class="img-container">
+            <img :src="product.thumb" alt="">
+        </div>
+        <div class="text-white text-uppercase py-2">
+            {{ product.series }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyCard',
+    props: {
+        product: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .img-container {
+        aspect-ratio: 1 / 1;
+        overflow: hidden;
+    }
+</style>
